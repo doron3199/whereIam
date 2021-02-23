95eaa7bcd1d00129196fdcf2858e551dcd274804
Doron Leibovici <doron3199@gmail.com>
2021-02-23 20:00:36 +0000

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws2/src
+# Build directory: /home/workspace/catkin_ws2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(first_package)
+subdirs(my_robot)
+subdirs(ball_chaser)
